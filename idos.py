676084267de61ee8c65833bd587fde65a8860261
Abdullah Al Rahat <131818049+abdullahalrahat@users.noreply.github.com>
2023-04-28 00:28:55 +0600

    Update idos.py

--- idos.py
+++ idos.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# python 3.3.2+ Hammer Dos Script v.1
-# by Can Yalçın
+# python 3.3.2+ Idos Dos Script v.1
+# by Abdullah
 # only for legal purpose
 
 
